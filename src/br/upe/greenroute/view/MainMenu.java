2e697e0583bcd2594854081f918e393f7923a0fa
jkdevs001 <kjose1809@gmail.com>
2026-06-07 06:38:28 -0300

    Update MainMenu.java

--- src/br/upe/greenroute/view/MainMenu.java
+++ src/br/upe/greenroute/view/MainMenu.java
@@ -54,8 +54,88 @@ public class MainMenu {
            }
         }
     }
-}
+}package br.upe.greenroute.model;
 
+public abstract class Veiculo {
+    private int id;
+    private String modelo;
+    private double autonomiaMaxima;
+    private double cargaBateriaAtual;
+    private double consumoKwhPorKm;
+    private int tempoRecargaCompleta;
+
+    public Veiculo(int id, String modelo, double autonomiaMaxima,
+            double cargaBateriaAtual, double consumoKwhPorKm,
+            int tempoRecargaCompleta) {
+        this.id = id;
+        this.modelo = modelo;
+        this.autonomiaMaxima = autonomiaMaxima;
+        this.cargaBateriaAtual = cargaBateriaAtual;
+        this.consumoKwhPorKm = consumoKwhPorKm;
+        this.tempoRecargaCompleta = tempoRecargaCompleta;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getAutonomiaMaxima() {
+        return autonomiaMaxima;
+    }
+
+    public void setAutonomiaMaxima(double autonomiaMaxima) {
+        this.autonomiaMaxima = autonomiaMaxima;
+    }
+
+    public double getCargaBateriaAtual() {
+        return cargaBateriaAtual;
+    }
+
+    public void setCargaBateriaAtual(double cargaBateriaAtual) {
+        this.cargaBateriaAtual = cargaBateriaAtual;
+    }
+
+    public double getConsumoKwhPorKm() {
+        return consumoKwhPorKm;
+    }
+
+    public void setConsumoKwhPorKm(double consumoKwhPorKm) {
+        this.consumoKwhPorKm = consumoKwhPorKm;
+    }
+
+    public int getTempoRecargaCompleta() {
+        return tempoRecargaCompleta;
+    }
+
+    public void setTempoRecargaCompleta(int tempoRecargaCompleta) {
+        this.tempoRecargaCompleta = tempoRecargaCompleta;
+    }
+
+    public double calcularAutonomiaAtual() {
+        return autonomiaMaxima * (cargaBateriaAtual / 100);
+    }
+
+    public void exibirDados() {
+        System.out.println("ID: " + id);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Autonomia Máxima: " + autonomiaMaxima + " km");
+        System.out.println("Carga da Bateria: " + cargaBateriaAtual + "%");
+        System.out.println("Consumo: " + consumoKwhPorKm + " kWh/km");
+        System.out.println("Tempo de Recarga Completa: " + tempoRecargaCompleta + " min");
+    }
+}
 package br.upe.greenroute.model;
 
 public class VeiculoEletrico extends Veiculo {
