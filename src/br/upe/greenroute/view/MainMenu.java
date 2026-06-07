af56810133e471bdd2d1148c3f2acfb622f2d621
jkdevs001 <kjose1809@gmail.com>
2026-06-07 06:33:50 -0300

    Update MainMenu.java

--- src/br/upe/greenroute/view/MainMenu.java
+++ src/br/upe/greenroute/view/MainMenu.java
@@ -70,3 +70,44 @@ public class Pessoa {
         System.out.println("Idade: " + idade);
     }
 }
+package br.upe.greenroute.model;
+
+public class VeiculoEletrico extends Veiculo {
+    private String tipoConector;
+    private int tempoRecargaRapida;
+
+    public VeiculoEletrico(int id, String modelo, double autonomiaMaxima,
+            double cargaBateriaAtual, double consumoKwhPorKm,
+            int tempoRecargaCompleta, String tipoConector,
+            int tempoRecargaRapida) {
+
+        super(id, modelo, autonomiaMaxima, cargaBateriaAtual,
+                consumoKwhPorKm, tempoRecargaCompleta);
+
+        this.tipoConector = tipoConector;
+        this.tempoRecargaRapida = tempoRecargaRapida;
+    }
+
+    public String getTipoConector() {
+        return tipoConector;
+    }
+
+    public void setTipoConector(String tipoConector) {
+        this.tipoConector = tipoConector;
+    }
+
+    public int getTempoRecargaRapida() {
+        return tempoRecargaRapida;
+    }
+
+    public void setTempoRecargaRapida(int tempoRecargaRapida) {
+        this.tempoRecargaRapida = tempoRecargaRapida;
+    }
+
+    @Override
+    public void exibirDados() {
+        super.exibirDados();
+        System.out.println("Tipo de Conector: " + tipoConector);
+        System.out.println("Tempo de Recarga Rápida: " + tempoRecargaRapida + " min");
+    }
+}
