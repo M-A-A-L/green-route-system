93c5c014dec5729c459fd4d41d29913ff8c9da17
jkdevs001 <kjose1809@gmail.com>
2026-06-07 06:36:17 -0300

    Update MainMenu.java

--- src/br/upe/greenroute/view/MainMenu.java
+++ src/br/upe/greenroute/view/MainMenu.java
@@ -56,20 +56,6 @@ public class MainMenu {
     }
 }
 
-public class Pessoa {
-    private String nome;
-    private int idade;
-
-    public Pessoa(String nome, int idade) {
-        this.nome = nome;
-        this.idade = idade;
-    }
-
-    public void exibirDados() {
-        System.out.println("Nome: " + nome);
-        System.out.println("Idade: " + idade);
-    }
-}
 package br.upe.greenroute.model;
 
 public class VeiculoEletrico extends Veiculo {
